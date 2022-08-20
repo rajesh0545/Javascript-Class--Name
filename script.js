ea4eef7cad1898eb6971a36c10fa8199e1c060f2
rajesh <krajeshgowri7032@mail.com>
2022-08-20 22:33:49 +0530

    Javascript Classname

---
+++ script.js
@@ -0,0 +1,10 @@
+
+ function fun(){
+     document.getElementById("img"). src="download.jpg";
+
+ }
+
+
+
+ const x=document.querySelectorAll("p.idel");
+document.getElementById("demo").innerHTML='Educatin is the passport to the future,For tomorrow belonges to those who prepare for it TODAY class="idel"' + x.innerHTML;
